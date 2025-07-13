12a2e3bccb1eef0bb8c62876585b731ffe9e1534
gaddganepola <ganeypola@gmail.com>
2025-07-13 21:36:14 +0530

    delete data in database

--- src/DeleteDataDemo.java
+++ src/DeleteDataDemo.java
@@ -1,5 +1,26 @@
+import java.sql.*;
+
 public class DeleteDataDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:postgresql://localhost:5432/JDBC_demo";
+        String username = "postgres";
+        String password = "29997";
+
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        System.out.println("Connection Established");
+
+        Statement stmt = con.createStatement();
+
+        String query = "DELETE FROM student WHERE id = 1";
+
+        boolean status = stmt.execute(query);
+
+        System.out.println(!status ?"Data deleted successfully" : "Error in deleting data");
+
+        con.close();
+
+        System.out.println("Connection closed");
 
     }
 }
