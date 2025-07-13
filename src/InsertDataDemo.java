b5b26012ce38e32651af6b8f9092b55327138d20
gaddganepola <ganeypola@gmail.com>
2025-07-13 21:24:05 +0530

    insert data in to database

---
+++ src/InsertDataDemo.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class InsertDataDemo {
+    public static void main(String[] args) throws Exception {
+
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://localhost:5432/JDBC_demo";
+        String username = "postgres";
+        String password = "29997";
+
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        Statement stmt = con.createStatement();
+
+        String query = "INSERT INTO student (name, marks) VALUES ('Malinda', 87)";
+
+        boolean status = stmt.execute(query);
+
+        System.out.println(!status ?"Data added successfully" : "Error in adding data");
+
+        con.close();
+
+
+
+
+    }
+}
